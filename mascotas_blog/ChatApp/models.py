f74cf4290563b6b5e8c4463cb0cdb4e0b35dc58e
Francisco Medina <medinafran2002@hotmail.com>
2023-02-09 19:56:23 -0300

    <feat: create models Chat and Message (before migration)>

--- mascotas_blog/ChatApp/models.py
+++ mascotas_blog/ChatApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from FeedApp.models import User
 
 # Create your models here.
+class Chat(models.Model):
+    id_chat = models.AutoField(primary_key=True)
+    user_1 = models.ForeignKey(User,)
+    user_2 = models.ForeignKey(User,)
+
+class Message(models.Model):
+    id_msg = models.AutoField(primary_key=True)
+    date = models.DateTimeField(auto_now=True, editable=False)
+    sender = models.ForeignKey(User, )
+    chat = models.ForeignKey(Chat, )
+    seen = models.BooleanField(default=False)
+    message = models.TextField()
