df6f91a21a32e80a76156e0a4e34189e001e9d0e
Francisco Medina <medinafran2002@hotmail.com>
2023-02-09 20:04:20 -0300

    <fix: <<ChatApp.Chat.user_1: (fields.E304) Reverse accessor User.chat_set for ChatApp.Chat.user_1 clashes with reverse accessor for ChatApp.Chat.user_2.>> fixed (before migration)>

--- mascotas_blog/ChatApp/models.py
+++ mascotas_blog/ChatApp/models.py
@@ -4,13 +4,13 @@ from FeedApp.models import User
 # Create your models here.
 class Chat(models.Model):
     id_chat = models.AutoField(primary_key=True)
-    user_1 = models.ForeignKey(User,)
-    user_2 = models.ForeignKey(User,)
+    user_1 = models.ForeignKey(User, on_delete= models.CASCADE, related_name='user_1')
+    user_2 = models.ForeignKey(User, on_delete= models.CASCADE, related_name='user_2')
 
 class Message(models.Model):
     id_msg = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now=True, editable=False)
-    sender = models.ForeignKey(User, )
-    chat = models.ForeignKey(Chat, )
+    sender = models.ForeignKey(User, on_delete= models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete= models.CASCADE)
     seen = models.BooleanField(default=False)
     message = models.TextField()
