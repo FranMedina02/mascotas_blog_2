1f53fa2902f82b741ad6d310cc13cd7e5cde71e8
Francisco Medina <medinafran2002@hotmail.com>
2023-03-14 22:29:19 -0300

    <refactor: change home url to feed; prepare to merge>

--- mascotas_blog/FeedApp/urls.py
+++ mascotas_blog/FeedApp/urls.py
@@ -4,7 +4,7 @@ import FeedApp.views as views
 
 
 urlpatterns = [
-    path('', views.home, name='Home'),
-    path('posts/', views.posts, name='Posts'),
+    path('', views.posts, name='Home'),
+    #path('posts/', views.posts, name='Posts'),
     path('posts/<id_post>', views.single_post, name='Single Post'),
 ]
