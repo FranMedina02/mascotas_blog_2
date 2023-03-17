33ae76e2ae004b45e9a90b1b37d76f64b991be78
Francisco Medina <medinafran2002@hotmail.com>
2023-03-17 19:57:06 -0300

    <no se>

--- mascotas_blog/FeedApp/forms.py
+++ mascotas_blog/FeedApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class PostFormulario(forms.Form):
-    #class = 'asd'
+
     title = forms.CharField(max_length=25)
     subtitle = forms.CharField(max_length=35)
     desc = forms.CharField(max_length=255)
