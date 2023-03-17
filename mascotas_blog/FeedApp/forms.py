a17a1b8c9137b4112d390e3a4e10bcaa4a80ca03
Francisco Medina <medinafran2002@hotmail.com>
2023-03-17 19:30:06 -0300

    <feat/refactor: overall quality>

--- mascotas_blog/FeedApp/forms.py
+++ mascotas_blog/FeedApp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class PostFormulario(forms.Form):
-
+    #class = 'asd'
     title = forms.CharField(max_length=25)
     subtitle = forms.CharField(max_length=35)
-    description = forms.CharField(max_length=255)
+    desc = forms.CharField(max_length=255)
     id_img = forms.ImageField()
 
--- mascotas_blog/FeedApp/views.py
+++ mascotas_blog/FeedApp/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from FeedApp.models import Post
 from FeedApp.forms import PostFormulario
+from UserApp.models import User
 
 def home(request):
 
@@ -12,9 +13,9 @@ def home(request):
     return render(request, template, context)
 
 
-def posts(request, id_post=None):
-
-    posts = Post.objects.all()
+def posts(request):
+    
+    posts = Post.objects.all().order_by('-id_post')
     template = 'FeedApp/post.html'
     context = {'posts':posts}
 
@@ -32,24 +33,26 @@ def single_post(request, id_post):
 def postFormulario(request):
 
     if request.method == 'POST':
-
-        form = PostFormulario(request.POST)
-
+        
+        form = PostFormulario(request.POST, request.FILES)
+        print(form)
         if form.is_valid():
 
             info = form.cleaned_data
+            print(info)
             post = Post(title = info['title'],
                         subtitle = info['subtitle'],
                         description = info['desc'],
-                        id_img = info['img'])
+                        id_user = User.objects.get(id_user = 1),
+                        id_img = info['id_img'])
             post.save()
 
-        return render(request, 'FeedApp/post.html')
+            return redirect('Home')
     
     else:
         form = PostFormulario()
 
-    return render(request, 'postFormulario.html', {'form':form})
+    return render(request, 'FeedApp/postFormulario.html', {'form':form})
 
 def search(request, data):
     pass
