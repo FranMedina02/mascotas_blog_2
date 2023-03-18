288f280a01b04b2d5f4af0b7dd42b5e1029d3d55
Francisco Medina <medinafran2002@hotmail.com>
2023-03-18 11:39:12 -0300

    <feat: create CustomUser from AbstractUser>

--- mascotas_blog/UserApp/models.py
+++ mascotas_blog/UserApp/models.py
@@ -1,12 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(models.Model):
-    id_user = models.AutoField(primary_key=True)
-    mail = models.EmailField(unique=True)
-    username = models.CharField(max_length=40, unique=True)
-    password = models.CharField(max_length=16)
+class CustomUser(AbstractUser):
     description = models.CharField(max_length=255)
 
     def __str__(self):
