ddd0c8a230d98c658e059c89b0ac09dc97ab4a36
Francisco Medina <medinafran2002@hotmail.com>
2023-04-03 19:14:47 -0300

    <feat: update views>

--- mascotas_blog/ChatApp/views.py
+++ mascotas_blog/ChatApp/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from ChatApp.models import Chat, Message
+from UserApp.models import CustomUser
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
-def conversation(request):
-    return redirect('Home')
+def conversation(request, conversation:int):
+    context = {}
+    return render(request, 'ChatApp/single_chat.html', context)
 def all_chats(request):
-    return redirect('Home')
+    context = {}
+    return render(request, 'ChatApp/all_chats.html', context)
