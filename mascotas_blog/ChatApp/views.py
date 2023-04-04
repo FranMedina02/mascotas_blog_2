08ff46ca3943bea65e9aec15243e48c63a917ebc
Francisco Medina <medinafran2002@hotmail.com>
2023-04-03 22:56:08 -0300

    <toFix: need to drop db and migrate>

--- mascotas_blog/ChatApp/views.py
+++ mascotas_blog/ChatApp/views.py
@@ -2,13 +2,32 @@ from django.shortcuts import render, redirect
 from ChatApp.models import Chat, Message
 from UserApp.models import CustomUser
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 
 
 # Create your views here.
+@login_required(login_url='/login/')
 def conversation(request, conversation:int):
     context = {}
+
+    
     return render(request, 'ChatApp/single_chat.html', context)
+
+
+@login_required(login_url='/login/')
 def all_chats(request):
+    user = request.user
     context = {}
+
+    genericUser = CustomUser.objects.get(id = 9)
+
+    chat1 = Chat(user_1 = user, user_2 = genericUser)
+    chat1.save()
+    chat2 = Chat(user_1 = genericUser, user_2 = user)
+    chat2.save()
+
+
+    chats = Chat.objects.filter(Q(user_1 = user) | Q(user_2 = user))
+    print(chats)
+
     return render(request, 'ChatApp/all_chats.html', context)
